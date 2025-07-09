30f92c58a3f7e9b099f67bf041e596a1d14b2670
oxy.szn <164936682+oxyactive@users.noreply.github.com>
2025-07-09 10:21:19 +0100

    Update effects2.js

--- effects2.js
+++ effects2.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://lanyard.cnrad.dev/api/71434390550355968?bg=00000000&animated=true=true&hideSpotify=false&showDisplayName=true&hideActivity=true";
+    const imgSrc = "https://lanyard.cnrad.dev/api/1225466516074397726?hideSpotify=true&hideTimestamp=true&hideDecoration=true";
     const imgElement = document.getElementById('rpc');
     imgElement.src = imgSrc;
 });
